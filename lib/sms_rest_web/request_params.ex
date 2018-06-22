8a0ae42614565f2b1ddfbb4216b3649bdd192cc4
Girdhar <gsojitra@shoretel.com>
2018-06-22 11:01:56 +0530

    fixing missing params

--- lib/sms_rest_web/request_params.ex
+++ lib/sms_rest_web/request_params.ex
@@ -33,36 +33,55 @@ defmodule SMSRestWeb.RequestParams do
       for {key, val} <- params, into: %{} do
         {String.to_existing_atom(key), val}
       end
+
     struct(%SMSRestWeb.RequestParams{}, params_with_atom_keys)
   end
 
   # TODO: validate principalId and content
   def changeset(%{"phoneNumber" => _number} = params) do
     data = %{}
+
     {data, types}
     |> cast(params, Map.keys(types))
     |> validate_required([:to, :content], message: "required")
     |> validate_format(:phoneNumber, @e164_regex)
     |> validate_format(:from, @e164_regex)
     |> validate_format(:to, @e164_regex)
-    |> validate_length(:content, max: @max_content, count: :codepoints, message: "contentTooLarge")
+    |> validate_length(
+      :content,
+      max: @max_content,
+      count: :codepoints,
+      message: "contentTooLarge"
+    )
   end
 
   def changeset(%{"userId" => _userId} = params) do
     data = %{}
+
     {data, types}
     |> cast(params, Map.keys(types))
     |> validate_required([:to, :content], message: "required")
     |> validate_format(:from, @e164_regex)
     |> validate_format(:to, @e164_regex)
-    |> validate_length(:content, max: @max_content, count: :codepoints, message: "contentTooLarge")
+    |> validate_length(
+      :content,
+      max: @max_content,
+      count: :codepoints,
+      message: "contentTooLarge"
+    )
   end
 
-  def find_missing_params_or_large_content({key, {"required", _}}, {_, isContentLarge, missingFields}) do
+  def find_missing_params_or_large_content(
+        {key, {"required", _}},
+        {_, isContentLarge, missingFields}
+      ) do
     {true, isContentLarge, "#{missingFields}, #{key}"}
   end
 
-  def find_missing_params_or_large_content({key, {"contentTooLarge", _}}, {isMissing, _, missingFields}) do
+  def find_missing_params_or_large_content(
+        {key, {"contentTooLarge", _}},
+        {isMissing, _, missingFields}
+      ) do
     {isMissing, true, "#{missingFields}, #{key}"}
   end
 
@@ -74,26 +93,32 @@ defmodule SMSRestWeb.RequestParams do
     # IO.inspect changeset
     {isMissing, isContentLarge, missingFieldswithComma} =
       Enum.reduce(changeset.errors, {false, false, ""}, &find_missing_params_or_large_content/2)
+
     if isMissing do
       ", " <> missingFields = missingFieldswithComma
       # TODO: missingFields can be added if required in response
-      {422, %ResponseError{
-        name: "MissingParams",
-        message: "one or more required parameters are missing or empty"
-      }}
-    end
-    if isContentLarge do
-      {413, %ResponseError{
-        name: "ContentTooLarge",
-        message: "content length is larger than allowed limit"
-      }}
+      {422,
+       %ResponseError{
+         name: "MissingParams",
+         message: "one or more required parameters are missing or empty"
+       }}
     else
-      ", " <> invalidParams =
-        Enum.reduce(changeset.errors, "", fn {key, value}, acc ->
-          "#{acc}, #{key}"
-        end)
-      # TODO: invalid fields can be added if required in response
-      {422, %ResponseError{name: "InvalidParams", message: "one or more parameters are invalid"}}
+      if isContentLarge do
+        {413,
+         %ResponseError{
+           name: "ContentTooLarge",
+           message: "content length is larger than allowed limit"
+         }}
+      else
+        ", " <> invalidParams =
+          Enum.reduce(changeset.errors, "", fn {key, value}, acc ->
+            "#{acc}, #{key}"
+          end)
+
+        # TODO: invalid fields can be added if required in response
+        {422,
+         %ResponseError{name: "InvalidParams", message: "one or more parameters are invalid"}}
+      end
     end
 
     # Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
